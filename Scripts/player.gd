ff89881a9805a1f08e55317ef0040518a55920fe
lgraham076 <langstongraham@gmail.com>
2026-06-22 22:15:32 -0500

    Animated Flappy

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -7,7 +7,6 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 func _ready() -> void:
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
 
@@ -15,10 +14,12 @@ func _physics_process(delta: float) -> void:
 	velocity.x = 1000.0
 
 	if Input.is_action_pressed("flap"):
+		$AnimatedSprite2D.animation = &"upflap"
 		velocity.y = -1000.0
 	else:
+		$AnimatedSprite2D.animation = &"downflap"
 		velocity.y = 2000.0
 		
 	
-
+	$AnimatedSprite2D.play()
 	move_and_slide()
